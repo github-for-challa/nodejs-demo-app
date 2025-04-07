564b3fbdb911e244f36f8ccf2a62e71724993b21
git-for-challa <srinivas.challa788@gmail.com>
2025-04-07 04:40:22 -0700

    Update response message to trigger pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Dockerized Node.js App!');
+  res.send('Hello from CI/CD Pipeline!');
 });
 
 const PORT = 3000;
